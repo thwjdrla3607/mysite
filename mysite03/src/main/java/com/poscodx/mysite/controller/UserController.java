14c1bf4dbb3f3c8dbb97b5e28d0678dc1c839158
sso <sojeong7524@gmail.com>
2023-09-22 14:37:01 +0900

    Validation #2

--- mysite03/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.poscodx.mysite.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,50 +18,60 @@ import com.poscodx.mysite.vo.UserVo;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	@Autowired
-	private UserService userService;
+   @Autowired
+   private UserService userService;
 
-	@RequestMapping(value="/join", method=RequestMethod.GET)
-	public String join() {
-		return "user/join";
-	}
+   @RequestMapping(value="/join", method=RequestMethod.GET)
+   public String join(@ModelAttribute UserVo userVo) {
+	   return "user/join";
+   }
 
-	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String join(UserVo userVo) {
-		userService.join(userVo);
-		return "redirect:/user/joinsuccess";
-	}
+   @RequestMapping(value="/join", method=RequestMethod.POST)
+   public String join(@ModelAttribute @Valid UserVo userVo, BindingResult result, Model model) {
+      if(result.hasErrors()) {
+//         List<ObjectError> list = result.getAllErrors();
+//         for(ObjectError error : list) {
+//            System.out.println(error);
+//         }
+         
+         model.addAllAttributes(result.getModel());
+         return "user/join";
+      }
+      
+      userService.join(userVo);
+      return "redirect:/user/joinsuccess";
+   }
 
-	@RequestMapping(value="/joinsuccess", method=RequestMethod.GET)
-	public String joinsuccess() {
-		return "user/joinsuccess";
-	}
+   @RequestMapping(value="/joinsuccess", method=RequestMethod.GET)
+   public String joinsuccess() {
+      return "user/joinsuccess";
+   }
 
-	@RequestMapping(value="/login", method=RequestMethod.GET)
-	public String login() {
-		return "user/login";
-	}
-	
-	@Auth
-	@RequestMapping(value="/update", method=RequestMethod.GET)
-	public String update(@AuthUser UserVo authUser, Model model) {		
-		UserVo userVo = userService.getUser(authUser.getNo());
-		model.addAttribute("userVo", userVo);
-		return "user/update";
-	}
-	
-	@Auth
-	@RequestMapping(value="/update", method=RequestMethod.POST)
-	public String update(@AuthUser UserVo authUser, UserVo userVo) {
-		userVo.setNo(authUser.getNo());
-		userService.update(userVo);
-		
-		authUser.setName(userVo.getName());
-		return "redirect:/user/update";
-	}
+   @RequestMapping(value="/login", method=RequestMethod.GET)
+   public String login() {
+      return "user/login";
+   }
+   
+   @Auth
+   @RequestMapping(value="/update", method=RequestMethod.GET)
+   public String update(@AuthUser UserVo authUser, Model model) {      
+      UserVo userVo = userService.getUser(authUser.getNo());
+      model.addAttribute("userVo", userVo);
+      return "user/update";
+   }
+   
+   @Auth
+   @RequestMapping(value="/update", method=RequestMethod.POST)
+   public String update(@AuthUser UserVo authUser, UserVo userVo) {
+      userVo.setNo(authUser.getNo());
+      userService.update(userVo);
+      
+      authUser.setName(userVo.getName());
+      return "redirect:/user/update";
+   }
 
-//	@ExceptionHandler(Exception.class)
-//	public String handlerException() {
-//		return "error/exception";
-//	}
+//   @ExceptionHandler(Exception.class)
+//   public String handlerException() {
+//      return "error/exception";
+//   }
 }
