e8e052ebfcbe818254afc2d5df61b9c6e127c3b9
sso <sojeong7524@gmail.com>
2023-09-25 11:40:57 +0900

    site title issue #finish

--- mysite03/src/main/java/com/poscodx/mysite/controller/AdminController.java
+++ mysite03/src/main/java/com/poscodx/mysite/controller/AdminController.java
@@ -2,7 +2,9 @@ package com.poscodx.mysite.controller;
 
 import javax.servlet.ServletContext;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,8 @@ import com.poscodx.mysite.vo.SiteVo;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	@Autowired
+	private ApplicationContext applicationContext;
 	
 	@Autowired
 	private ServletContext servletContext;
@@ -40,8 +44,17 @@ public class AdminController {
 		if(profile != null) {
 			vo.setProfile(profile);
 		}		
-		siteService.updateSite(vo);
 		
+		SiteVo site = applicationContext.getBean(SiteVo.class);
+
+		siteService.updateSite(vo);
+		servletContext.setAttribute("siteVo", vo);
+		
+//		site.setTitle(vo.getTitle());
+//		site.setWelcome(vo.getWelcome());
+//		site.setProfile(vo.getProfile());
+//		site.setDescription(vo.getDescription());
+		BeanUtils.copyProperties(vo, site);
 		
 		return "redirect:/admin";
 	}
