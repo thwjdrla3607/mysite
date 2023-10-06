17f95fd1e7b8f808829481286fd5e3263b55d84e
sso <sojeong7524@gmail.com>
2023-10-06 13:14:14 +0900

    mysite05 #1

---
+++ mysite05/src/main/java/com/poscodx/mysite/initializer/MySiteWebApplicationInitializer.java
@@ -0,0 +1,33 @@
+package com.poscodx.mysite.initializer;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.poscodx.mysite.config.AppConfig;
+import com.poscodx.mysite.config.WebConfig;
+
+public class MySiteWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] { AppConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		return new Filter[] { new CharacterEncodingFilter("utf-8") };
+	}
+
+}
